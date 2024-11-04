210d12deea7ce8b59247c2a5ff1ac42bab59c1dd
ThiagoxMedeiros <thiagoxmedeiros@gmail.com>
2024-11-04 19:19:04 -0300

    commit da versao final - foram introzidos os laõs em whilee foreach alem do metodo de decisao com switch

--- index.js
+++ index.js
@@ -6,47 +6,151 @@ Se XP for entre 7.001 e 8.000 = Platina
 Se XP for entre 8.001 e 9.000 = Ascendente
 Se XP for entre 9.001 e 10.000= Imortal
 Se XP for maior ou igual a 10.001 = Radiante*/
-let nivelHeroi = ""
+
 let heroi = [
-  {nomeHeroi:'Link', qtdXp: 999},
+  {nomeHeroi:'Link', qtdXp:999},
   {nomeHeroi:'Kratos', qtdXp:2001},
   {nomeHeroi:'Seya', qtdXp:1500},
   {nomeHeroi:'Shaka', qtdXp:7000},
   {nomeHeroi:'Goku', qtdXp:10001},
 ]
+let nivelHeroi = 0;
+let rankHeroi ="";
+let i = 0;
+let nome =""
 
-for(let i=0;i<=heroi.length;i++){
-  if(heroi[i].qtdXp >= 0 && heroi[i].qtdXp <=1000){
-    nivelHeroi = "Ferro"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
-  }else if(heroi[i].qtdXp >= 1001 && heroi[i].qtdXp <= 2000){
-    nivelHeroi = "Bronze"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
-  }else if(heroi[i].qtdXp >= 2001 && heroi[i].qtdXp <= 5000){
-     nivelHeroi = "Prata"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
-  }else if(heroi[i].qtdXp >= 5001 && heroi[i].qtdXp <= 7000){
-    nivelHeroi = "Ouro"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
-  }else if(heroi[i].qtdXp >= 7001 && heroi[i].qtdXp <= 8000){
-     nivelHeroi = "Platina"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
-  }else if(heroi[i].qtdXp >= 8001 && heroi[i].qtdXp <= 9000){
-    nivelHeroi = "Ascendente"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
-  }else if(heroi[i].qtdXp >= 9001 && heroi[i].qtdXp <= 10000){
-    nivelHeroi = "Imortal"
-    nome = heroi[i].nomeHeroi 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
+// CRIANDO LAÇO DE REPETIÇÃO COM FOR
+/*for(let i=0;i<heroi.length;i++){
+ let nome = heroi[i].nomeHeroi 
+  nivelHeroi = heroi[i].qtdXp
+  if(nivelHeroi >= 0 && nivelHeroi <=1000){
+    rankHeroi = "Ferro"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+  }else if(nivelHeroi >= 1001 && nivelHeroi <= 2000){
+    rankHeroi = "Bronze"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+  }else if(nivelHeroi >= 2001 && nivelHeroi <= 5000){
+    rankHeroi = "Prata"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+  }else if(nivelHeroi >= 5001 && nivelHeroi <= 7000){
+    rankHeroi = "Ouro"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+  }else if(nivelHeroi >= 7001 && nivelHeroi <= 8000){
+    rankHeroi = "Platina"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+  }else if(nivelHeroi >= 8001 && nivelHeroi <= 9000){
+    rankHeroi = "Ascendente"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);;
+  }else if(nivelHeroi >= 9001 && nivelHeroi <= 10000){
+    rankHeroi = "Imortal"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
   }else{
-     nivelHeroi = "Radiante";
-    nome = heroi[i].nomeHeroi; 
-    console.log(`O herói ${nome} é nível ${nivelHeroi}!`);
+    rankHeroi = "Radiante"
+    console.log(`O herói ${nome} é nível ${rankHeroi}!`);
   }
-}
+}*/
+
+//FAZENDO LAÇO DE REPETIÇÃO COM WHILE
+/*while(i < heroi.length){
+  let nome = heroi[i].nomeHeroi 
+   nivelHeroi = heroi[i].qtdXp
+   if(nivelHeroi >= 0 && nivelHeroi <=1000){
+     rankHeroi = "Ferro"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 1001 && nivelHeroi <= 2000){
+     rankHeroi = "Bronze"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 2001 && nivelHeroi <= 5000){
+     rankHeroi = "Prata"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 5001 && nivelHeroi <= 7000){
+     rankHeroi = "Ouro"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 7001 && nivelHeroi <= 8000){
+     rankHeroi = "Platina"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 8001 && nivelHeroi <= 9000){
+     rankHeroi = "Ascendente"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);;
+   }else if(nivelHeroi >= 9001 && nivelHeroi <= 10000){
+     rankHeroi = "Imortal"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else{
+     rankHeroi = "Radiante"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }
+   i = i + 1
+ }*/
+
+ //FAZENDO LAÇO DE REPETIÇÃO COM FOREACH
+ /* heroi.forEach(item =>{
+  let nome = heroi[i].nomeHeroi 
+   nivelHeroi = heroi[i].qtdXp
+   if(nivelHeroi >= 0 && nivelHeroi <=1000){
+     rankHeroi = "Ferro"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 1001 && nivelHeroi <= 2000){
+     rankHeroi = "Bronze"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 2001 && nivelHeroi <= 5000){
+     rankHeroi = "Prata"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 5001 && nivelHeroi <= 7000){
+     rankHeroi = "Ouro"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 7001 && nivelHeroi <= 8000){
+     rankHeroi = "Platina"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else if(nivelHeroi >= 8001 && nivelHeroi <= 9000){
+     rankHeroi = "Ascendente"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);;
+   }else if(nivelHeroi >= 9001 && nivelHeroi <= 10000){
+     rankHeroi = "Imortal"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }else{
+     rankHeroi = "Radiante"
+     console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+   }
+   i = i + 1
+  })*/
+
+  // FAZENDO A ESTRUTURA DE DECIÇÃO COM SWITH CASE
+  heroi.forEach(item =>{
+    nome = heroi[i].nomeHeroi;
+    nivelHeroi = heroi[i].qtdXp;
+    i = i +1;
+    switch (true){ 
+      case (nivelHeroi >=0 && nivelHeroi <= 1000):
+        rankHeroi = "Ferro";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+        break;
+      case (nivelHeroi >= 1001 && nivelHeroi <=2000):
+        rankHeroi = "bronze";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+        break;
+        case (nivelHeroi >= 2001 && nivelHeroi <=5000):
+        rankHeroi = "prata";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+        break;
+        case (nivelHeroi >= 5001 && nivelHeroi <=7000):
+        rankHeroi = "Ouro";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+        break;
+        case (nivelHeroi >= 7001 && nivelHeroi <=8000):
+        rankHeroi = "Platina";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+        break;
+        case (nivelHeroi >= 8001 && nivelHeroi <=9000):
+        rankHeroi = "Ascedente";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+        break;
+        case (nivelHeroi >= 9001 && nivelHeroi <=10000):
+          rankHeroi = "Imortal";
+          console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+          break;
+      
+      default:
+        rankHeroi = "Radiante";
+        console.log(`O herói ${nome} é nível ${rankHeroi}!`);
+     }
+    })
